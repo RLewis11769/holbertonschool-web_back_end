e9f3e4cc365a8eb0e63cbe4165b334c3ed852b11
Rachel Lewis <RLewis11769@gmail.com>
2021-07-12 19:05:29 +0000

    Add data to 2-arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,11 @@
+// Use arrow function to replace add() with anonymous function
+
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const self = this;
+    this.addNeighborhood = (newNeighborhood) => {
+        self.sanFranciscoNeighborhoods.push(newNeighborhood);
+        return self.sanFranciscoNeighborhoods;
+    };
+}
