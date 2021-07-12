a95e12bab2542faaf53ef955763fe52d0ad26ead
Rachel Lewis <RLewis11769@gmail.com>
2021-07-12 22:13:53 +0000

    Remove comment from 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
-// Change var variables so not overwritten, pay attention to hoisting
-
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
