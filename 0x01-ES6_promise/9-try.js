a14a9f0da0140c1517d3b098f4f94f0938a92b0c
Rachel Lewis <RLewis11769@gmail.com>
2021-07-14 17:16:54 +0000

    Run eslint and modify accordingly

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathFunction) {
   try {
-    return [mathFunction(), 'Guardrail was processed']
+    return [mathFunction(), 'Guardrail was processed'];
   } catch (e) {
-    return [`${e.name}: ${e.message}`, 'Guardrail was processed']
+    return [`${e.name}: ${e.message}`, 'Guardrail was processed'];
   }
 }
