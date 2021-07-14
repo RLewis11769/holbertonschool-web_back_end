1000e976f89c502c1c2664566d0de8b0352fd7d3
Rachel Lewis <RLewis11769@gmail.com>
2021-07-14 20:38:44 +0000

    How in the world did the last one give me anything - add basic then/catch/finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,18 +1,6 @@
-export default function getFullResponseFromAPI(success) {
-  const promise = new Promise((resolve) => {
-    if (success) {
-      resolve({ status: 200, body: 'success' });
-    }
-    else {
-      console.log('Got a response from the API');
-      return Error();
-    }
-  });
-
-  promise
-  .then(() => {
-    console.log('Got a response from the API');
-    return promise;
-  });
-
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then({ status: 200, body: 'success' })
+    .catch(Error())
+    .finally(console.log('Got a response from the API'));
 }
