c5e78ca648f366b031b8ceb68f9f49d3d620c1a4
Rachel Lewis <RLewis11769@gmail.com>
2021-07-15 16:47:51 +0000

    Remove print just in case

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,6 +6,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     .then((data) => {
       data[1].value = `Error: ${data[1].reason.message}`;
       delete data[1].reason;
-      console.log(data)
     });
 }
