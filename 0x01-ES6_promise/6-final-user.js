8fea980e04b9d8c69256ffa55fdc8bf125bb3a69
Rachel Lewis <RLewis11769@gmail.com>
2021-07-15 16:35:54 +0000

    Rewrite to only include error message for value if rejected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((data) => {
-    return data
-  })
+    .then((data) => {
+      data[1].value = data[1].reason.message 
+      delete data[1].reason
+    })
 }
