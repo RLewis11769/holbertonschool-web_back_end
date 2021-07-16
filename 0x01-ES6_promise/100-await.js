b5dcdc8b39d880aa3a5a3ff268098273b01630b2
Rachel Lewis <RLewis11769@gmail.com>
2021-07-16 12:42:09 +0000

    Just an object not a dict inside a list

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,11 +3,13 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   return Promise.allSettled([uploadPhoto(), createUser()])
     .then((data) => {
-      const arr = [];
+      const arr = {};
       if (data[0].status === 'fulfilled' || data[1].status === 'fulfilled') {
-        arr.push({ photo: data[0].value, user: data[1].value });
+        arr.photo = data[0].value;
+        arr.user = data[1].value;
       } else {
-        arr.push({ photo: null, user: null });
+        arr.photo = null;
+        arr.user = null;
       }
       return arr;
     });
