64194b8546ed06dfcd88be97782617b55a8987a9
Rachel Lewis <RLewis11769@gmail.com>
2021-07-19 18:22:31 +0000

    Add data to 1-make_classrooms.js and add comments

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,5 @@
 export default class ClassRoom {
+  // Implement ClassRoom class that accepts maxStudentsSize attribute
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
