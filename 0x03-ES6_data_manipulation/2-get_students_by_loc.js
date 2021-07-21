6719642e3dc3dfcfcc024018cc0a57f3732526bc
Rachel Lewis <RLewis11769@gmail.com>
2021-07-21 18:56:22 +0000

    Learn proper arrow syntax and look up name of key

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(listStudents, city) {
   // Return array of objects from listStudents with specified city
-  return listStudents.filter((student) => student.city === city);
+  return listStudents.filter((student) => student.location === city);
 }
