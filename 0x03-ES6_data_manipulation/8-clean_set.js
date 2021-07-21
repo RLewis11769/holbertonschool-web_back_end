2a449cac8d15db7520df826ebc80aa58fefa309f
Rachel Lewis <RLewis11769@gmail.com>
2021-07-21 23:34:09 +0000

    Change return for empty string to no do anything

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(setToCheck, startStr) {
+  // If values in setToCheck start with startStr, return values without startStr
   if (startStr === '') {
-    return startStr;
+    return '';
   }
   const arr = [];
-  for (const i of setToCheck) {
-    if (i.startsWith(startStr)) {
-      arr.push(i.slice(startStr.length));
+  for (const val of setToCheck) {
+    if (val.startsWith(startStr)) {
+      arr.push(val.slice(startStr.length));
     }
   }
   return arr.join('-');
