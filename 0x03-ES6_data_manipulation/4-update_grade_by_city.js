f6f5b032597d0aea6efcf201bd67c870f004f523
Rachel Lewis <RLewis11769@gmail.com>
2021-07-25 21:42:40 +0000

    Update grade in student obj to newGrades.grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,14 +2,24 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
   // Returns array of students from specified city with new grade added
   // note: newGrades is array of grade objects that contains studentid and grade
 
+  // Returns list with changes made based on filter/map
   return studentList
+    // Filters down to only students where location matches
     .filter((student) => student.location === city)
+    // Does everything in function to every student in studentList
     .map((student) => {
-      let newGrade = 'N/A';
-      if (student.id === 1) {
-        newGrade = 5
-      }
-      return { ...student, grade: newGrade }
+      // Default of new_grade is N/A
+      let addGrade = 'N/A';
+      // Go through newGrades array and find studentId that matches this student
+      newGrades.forEach((grade) => {
+        if (grade.studentId === student.id) {
+          // Change addGrade from N/A to grade in newGrades
+          addGrade = grade.grade;
+        }
+      });
+      // console.log(obj);
+      console.log(addGrade);
+      // Returns everything in each student obj with grade from addGrade added to obj
+      return { ...student, grade: addGrade };
     });
-    
 }
