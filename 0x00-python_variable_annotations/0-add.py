863441c768361d403b11a262ecc006302690d606
Rachel Lewis <RLewis11769@gmail.com>
2021-11-02 17:26:54 +0000

    Newlines after file doc string

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """ Type-annotated function """
+
+
 def add(a: float, b: float) -> float:
     """ Function that takes floats as args and returns sum as float """
     return a + b
