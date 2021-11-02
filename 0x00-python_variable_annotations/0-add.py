fcb5aa7d284b6786f2fbf498864b1a5d75fc86af
Rachel Lewis <RLewis11769@gmail.com>
2021-11-02 17:26:00 +0000

    Doc string for file too

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Type-annotated function """
 def add(a: float, b: float) -> float:
     """ Function that takes floats as args and returns sum as float """
     return a + b
