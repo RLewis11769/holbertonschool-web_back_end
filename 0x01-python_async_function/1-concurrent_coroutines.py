efea517dd78038399194a0b6fe6cd2be72121fa8
Rachel Lewis <RLewis11769@gmail.com>
2021-11-03 20:31:54 +0000

    Update comments and doc - mypy doesn't like it

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,19 +8,19 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Async function that waits random time between 0 and max_delay """
+    """ Async function that calls wait_random n number of times """
     # wait_list holds coroutine objects holding floats
-    wait_list: List = []
+    task_list: List = []
     # complete_list holds floats
     complete_list: List[float] = []
 
     for x in range(n):
-        # wait_random asyncronously starts tasks
-        wait_list.append(asyncio.create_task(wait_random(max_delay)))
+        # wait_random asyncronously starts each task
+        task_list.append(asyncio.create_task(wait_random(max_delay)))
     # As each task is completed, it's waited for and then appended
     # wait is just a coroutine object containing float
-    for wait in asyncio.as_completed(wait_list):
-        completed: float = await wait
+    for task in asyncio.as_completed(task_list):
+        completed: float = await task
         # Now completed can be accessed - this can't be one line
         complete_list.append(completed)
 
