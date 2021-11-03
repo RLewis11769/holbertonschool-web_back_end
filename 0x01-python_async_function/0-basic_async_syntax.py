333afb31bbb7f76b069d17e71d91f26318a16246
Rachel Lewis <RLewis11769@gmail.com>
2021-11-03 19:19:31 +0000

    Proper annotation and documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+""" Type-annotated asynchronous coroutine """
 import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay=10):
-    random = uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    """ Async function that waits random time between 0 and max_delay """
+    random: float = uniform(0, max_delay)
     await asyncio.sleep(random)
     return random
