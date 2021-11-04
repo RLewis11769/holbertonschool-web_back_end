013941b100783b8202104e3d310c90e432736c44
Rachel Lewis <RLewis11769@gmail.com>
2021-11-04 20:38:28 +0000

    pep8 and annotate

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 """ Type-annotated asynchronous coroutine """
+
 from random import uniform
 from asyncio import sleep
+from typing import AsyncGenerator
 
-async def async_generator():
-	""" Async function that waits 1 second then yields random number 10 times """
-	for x in range(10):
-		await sleep(1)
-		yield uniform(1, 10)
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Async function that waits 1 second then yields random num 10 times """
+    for x in range(10):
+        # Asyncio sleep is non-blocking while time sleep is blocking
+        await sleep(1)
+        yield uniform(1, 10)
