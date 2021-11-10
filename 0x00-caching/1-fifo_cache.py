91df3b0d971754bb433577f7e716c3dc646bf80d
Rachel Lewis <73726775+RLewis11769@users.noreply.github.com>
2021-11-10 14:01:34 -0600

    Rewrite to use OrderedDict instead of list

--- 0x00-caching/1-fifo_cache.py
+++ 0x00-caching/1-fifo_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Module that defines BasicCache class/caching system """
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
@@ -7,24 +8,19 @@ class FIFOCache(BaseCaching):
     """ Class that inherits from BaseCaching and implements FIFO cache """
 
     def __init__(self):
-        """ Init new instance variable and keep any orig variables """
-        super().__init__()
-        self.cache_list = []
+        """ Override cache_data as OrderedDict not dict """
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Put method to assign item value to key """
+        """ Put method to assign item value to key and delete first-in """
         if (key and item):
             self.cache_data[key] = item
-            # List contains all keys found in dict in order
-            if key not in self.cache_list:
-                self.cache_list.append(key)
             if len(self.cache_data) > self.MAX_ITEMS:
-                # pop deletes and returns key at index
-                discarded = self.cache_list.pop(0)
-                print('DISCARD: {}'.format(discarded))
-                del self.cache_data[discarded]
+                # popitem deletes and returns dict at first index (in this case)
+                k, v = self.cache_data.popitem(last=False)
+                print('DISCARD: {}'.format(k))
 
     def get(self, key):
         """ Get method to return value at key """
-        if key:
+        if key or key not in self.cache_data:
             return self.cache_data.get(key)
