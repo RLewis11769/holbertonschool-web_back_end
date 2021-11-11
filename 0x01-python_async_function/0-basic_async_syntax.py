f61642d724ca457eeb0298f9410e39764b6f2824
Rachel Lewis <RLewis11769@gmail.com>
2021-11-11 03:40:03 +0000

    Parameter is int - don't indicate optional

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import Optional
 
 
-async def wait_random(max_delay: Optional[int] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ Async function that waits random time between 0 and max_delay """
     random: float = uniform(0, max_delay)
     await asyncio.sleep(random)
