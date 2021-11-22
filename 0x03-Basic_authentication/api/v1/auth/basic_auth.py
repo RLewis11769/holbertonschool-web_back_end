c5aa75e10909962b17f0a6773db15077e0e02f84
Rachel Lewis <RLewis11769@gmail.com>
2021-11-22 01:40:37 +0000

    Attempt 8 with try/except

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Class to manage Basic API authentication """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 from flask import request
 from typing import List, TypeVar
 
@@ -17,3 +18,16 @@ class BasicAuth(Auth):
                 if authorization_header.startswith("Basic "):
                     return (authorization_header[6:])
         return (None)
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ Decode encoded authorization header """
+        # if base64_authorization_header is None:
+        #     if type(base64_authorization_header) is not str:
+        #         return(None)
+        try:
+            # Decode base64 bytes to utf-8 string
+            return b64decode(base64_authorization_header).decode("utf-8")
+        except Exception:
+            return(None)
