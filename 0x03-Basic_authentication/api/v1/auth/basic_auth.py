938ed48a8dd354e05135257968865f5a3f7b51bf
Rachel Lewis <RLewis11769@gmail.com>
2021-11-22 02:11:39 +0000

    Once again, everything is hideous with pycodestyle because I wanted one if

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,9 +49,9 @@ class BasicAuth(Auth):
             decoded_base64_authorization_header: Decoded authorization header
         """
         # Edge cases that return (None, None) failure message
-        if decoded_base64_authorization_header is None or \
-        type(decoded_base64_authorization_header) is not str or \
-        ":" not in decoded_base64_authorization_header:
+        if (decoded_base64_authorization_header is None or
+            type(decoded_base64_authorization_header) is not str or
+           ":" not in decoded_base64_authorization_header):
             return(None, None)
         else:
             # Note: authorization header is in the form "email:password"
