847b93e2a88d9e4649ea226ffdce46ec4f06790c
Rachel Lewis <RLewis11769@gmail.com>
2021-11-22 01:30:07 +0000

    Make code much uglier with pycodestyle indention

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,10 +8,12 @@ from typing import List, TypeVar
 class BasicAuth(Auth):
     """ Basic Auth class """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """ Returns Base64 part of Authorization header """
         # Note: authorization header starts with "Basic "
-        if authorization_header is not None and type(authorization_header) is str:
-            if authorization_header.startswith("Basic "):
-                return (authorization_header[6:])
+        if authorization_header is not None:
+            if type(authorization_header) is str:
+                if authorization_header.startswith("Basic "):
+                    return (authorization_header[6:])
         return (None)
