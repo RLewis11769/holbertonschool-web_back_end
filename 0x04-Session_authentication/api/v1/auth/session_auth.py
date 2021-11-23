e68a68229ad68c8518302f29789f4ba984da5541
Rachel Lewis <RLewis11769@gmail.com>
2021-11-23 21:29:17 +0000

    Add data for task 3

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -16,10 +16,21 @@ class SessionAuth(Auth):
         Adds session id as key of dict with user id as value
 
         Args:
-            user_id: given user id to create session id for
+            user_id: given user id to create session id for, used as value
         """
         if user_id is None or type(user_id) is not str:
             return (None)
         session_id = str(uuid4())
         self.user_id_by_session_id.update({session_id: user_id})
         return (session_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Return user id based on session id
+
+        Args:
+            session_id: session id to get user id for
+        """
+        if session_id is None or type(session_id) is not str:
+            return (None)
+        return (self.user_id_by_session_id.get(session_id))
