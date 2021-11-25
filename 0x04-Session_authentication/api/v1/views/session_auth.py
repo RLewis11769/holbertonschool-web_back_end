c0fb9debf328051d01d724cba3c9f031050cda5d
Rachel Lewis <RLewis11769@gmail.com>
2021-11-25 02:56:32 +0000

    Don't need to get user object twice - thanks Jay

--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Module of Users views """
-from werkzeug.wrappers import response
 from api.v1.views import app_views
 from api.v1.auth.session_auth import SessionAuth
 from flask import abort, jsonify, request, make_response
@@ -34,11 +33,8 @@ def login():
         else:
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
-            user_id = auth.user_id_for_session_id(session_id)
-            # Find user instance based on user id
-            user_obj = User.get(user_id)
             # Create a response object by jsonifying the user object
-            response = jsonify(user_obj.to_json())
+            response = jsonify(user.to_json())
             # Set cookie to session id
             response.set_cookie(getenv('SESSION_NAME'), session_id)
             return (response)
