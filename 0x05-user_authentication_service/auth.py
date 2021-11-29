b6b534dd965308d9702d6e12196e21b84e168aae
Rachel Lewis <RLewis11769@gmail.com>
2021-11-29 21:04:08 +0000

    Add data for task 5 - register_user

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -1,4 +1,31 @@
 from bcrypt import hashpw, gensalt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+
+
+class Auth:
+    """ Auth class to interact with the authentication database """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers new user in the database and returns object if new user
+
+        Args:
+            email: user's email
+            password: user's password
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            pw = _hash_password(password)
+            user = self._db.add_user(email, pw)
+            return (user)
+
 
 def _hash_password(password: str) -> bytes:
     """
@@ -8,5 +35,4 @@ def _hash_password(password: str) -> bytes:
         password: password to hash
     """
     pw = password.encode('utf-8')
-    salt = gensalt()
-    return (hashpw(pw, salt))
+    return (hashpw(pw, gensalt()))
