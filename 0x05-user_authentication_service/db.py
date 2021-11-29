7df7ff077eb80c0d960110112918d549f28d4483
Rachel Lewis <RLewis11769@gmail.com>
2021-11-29 20:10:36 +0000

    Add data for task 2

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ DB module """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
@@ -28,7 +30,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add new user to database and returns object
-        
+
         Args:
             email: user's email
             hashed_password: hashed password
@@ -37,3 +39,17 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return (user)
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """
+        Finds user by given args and return first row as filtered by input
+
+        Args:
+            kwargs: dict of args to filter by
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
