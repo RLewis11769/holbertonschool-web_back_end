4166c1973aefa08f8ce291a4913f6436ba946020
Rachel Lewis <RLewis11769@gmail.com>
2021-12-01 19:19:40 +0000

    Add data for /profile GET - task 15

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -55,5 +55,16 @@ def logout():
         abort(403)
 
 
+@app.route("/profile", methods=['GET'])
+def profile():
+    """ Route to get user profile from session_id cookie """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user and session_id:
+        return jsonify({'email': user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
