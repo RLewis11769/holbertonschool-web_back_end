d1bca41407526f394aadecf3474dc640a762dfca
Rachel Lewis <RLewis11769@gmail.com>
2021-12-01 20:02:56 +0000

    Correct method name - test next time

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def update_password():
         email = request.form.get('email')
         reset_token = request.form.get('reset_token')
         new_password = request.form.get('new_password')
-        AUTH.reset_password(reset_token, new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({'email': email, 'message': 'Password updated'}), 200
     except Exception:
         abort(403)
