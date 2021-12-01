0c0dcac93c37769b3ae272d50c67a7d27c3ceb9a
Rachel Lewis <RLewis11769@gmail.com>
2021-12-01 01:48:51 +0000

    Add logout function for delete /sessions - task 14

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Authorization module """
 from auth import Auth
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, redirect, request
 
 
 AUTH = Auth()
@@ -41,6 +41,18 @@ def login():
     response = jsonify({'email': email, 'message': 'logged in'})
     response.set_cookie('session_id', session_id)
     return (response)
+
+
+@app.route("/sessions", methods=['DELETE'])
+def logout():
+    """ Route to delete session_id cookie and redirect """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
