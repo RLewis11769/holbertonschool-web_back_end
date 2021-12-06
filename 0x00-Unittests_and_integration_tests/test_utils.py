c932e2c5cbcfe83c5808b94a3686e417e0b49d52
Rachel Lewis <RLewis11769@gmail.com>
2021-12-06 20:16:57 +0000

    Add key error tests for access_nsted_map - task 1

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -8,11 +8,21 @@ class TestAccessNestedMap(TestCase):
     """ Test for nested map methods """
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ Test for access_nested_map function """
         from utils import access_nested_map
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b")),
+        ({"a": {"b": 2}}, ("b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Test for failures of access_nested_map function """
+        from utils import access_nested_map
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
