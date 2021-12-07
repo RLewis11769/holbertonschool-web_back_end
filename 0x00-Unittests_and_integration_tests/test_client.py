1779fa416cafefa6324108ff30fc2910a6994c1e
Rachel Lewis <RLewis11769@gmail.com>
2021-12-07 03:30:33 +0000

    Add test for _public_repos_url - task 5

--- 0x00-Unittests_and_integration_tests/test_client.py
+++ 0x00-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """ Unit tests for client.py """
+from client import GithubOrgClient
 from parameterized import parameterized
-from unittest import TestCase, mock
+from unittest import TestCase
+from unittest.mock import patch, MagicMock, PropertyMock
 
 
 class TestGithubOrgClient(TestCase):
@@ -11,10 +13,22 @@ class TestGithubOrgClient(TestCase):
         ("google"),
         ("abc")
     ])
-    @mock.patch('client.get_json',
-                mock.MagicMock(return_value={'key': 'value'}))
+    # MagicMock creates attributes and methods
+    # org returns jsonized dict - so specify return_value dict
+    @patch('client.get_json',
+           MagicMock(return_value={'key': 'value'}))
     def test_org(self, org_name):
-        """ Test for GithubOrgClient.org and .get_json methods """
-        from client import GithubOrgClient
-        test = GithubOrgClient(org_name)
-        self.assertEqual(test.org, {'key': 'value'})
+        """ Test for GithubOrgClient.org method """
+        cls = GithubOrgClient(org_name)
+        self.assertEqual(cls.org, {'key': 'value'})
+
+    def test_public_repos_url(self):
+        """ Test for GithubOrgClient._public_repos_url method """
+        # PropertyMock provides getter/setter methods and calls with no args
+        # _public_repos_url returns value for key repos_url
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value={'repos_url': 'url'}):
+            # Method doesn't take args but need to mock with org_name for org
+            cls = GithubOrgClient('org_name')
+            self.assertEqual(cls._public_repos_url, 'url')
