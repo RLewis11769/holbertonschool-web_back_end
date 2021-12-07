b569c5c060de1ce4ffc2f08266ce1f8cbafc2083
Rachel Lewis <RLewis11769@gmail.com>
2021-12-07 05:14:35 +0000

    Clean up test_utils.py and update readme

--- 0x00-Unittests_and_integration_tests/test_utils.py
+++ 0x00-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """ Unit tests for utils.py """
 from parameterized import parameterized
-from unittest import TestCase, mock
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(TestCase):
     """ Tests for access_nested_map function """
 
+    # tuple of (nested_map, path, expected value)
     @parameterized.expand([
         ({"a": 1}, ("a"), 1),
         ({"a": {"b": 2}}, ("a"), {"b": 2}),
@@ -14,9 +17,9 @@ class TestAccessNestedMap(TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ Test for access_nested_map function """
-        from utils import access_nested_map
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    # tuple of (nested_map, path) - expect KeyError
     @parameterized.expand([
         ({}, ("a")),
         ({"a": 1}, ("a", "b")),
@@ -24,22 +27,22 @@ class TestAccessNestedMap(TestCase):
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """ Test for key error failures of access_nested_map function """
-        from utils import access_nested_map
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
 class TestGetJson(TestCase):
     """ Tests for get_json function """
-
+    # tuple of (url, payload dict)
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, url, payload):
         """ Test for get_json returning payload """
-        from utils import get_json
-        with mock.patch("requests.get") as req:
+        with patch("requests.get") as req:
+            # Set return value of requests.get to payload
             req().json.return_value = payload
+            # Verify that when pass in url, recieve value of payload
             self.assertEqual(get_json(url), payload)
 
 
@@ -48,7 +51,6 @@ class TestMemoize(TestCase):
 
     def test_memoize(self):
         """ Test for asserting that memoize function sets attr """
-        from utils import memoize
 
         class TestClass:
             """ Helper class to test with specific return value """
@@ -61,14 +63,14 @@ class TestMemoize(TestCase):
 
         # MagicMock to mock method/return value
         test1 = TestClass()
-        test1.a_method = mock.MagicMock(return_value=42)
+        test1.a_method = MagicMock(return_value=42)
         self.assertEqual(test1.a_property, 42)
         self.assertEqual(test1.a_property, 42)
         test1.a_method.assert_called_once()
 
         # patch.object to mock method/return value
-        with mock.patch.object(TestClass, "a_method",
-                               return_value=42) as mock_method:
+        with patch.object(TestClass, "a_method",
+                          return_value=42) as mock_method:
             test2 = TestClass()
             self.assertEqual(test2.a_property, 42)
             self.assertEqual(test2.a_property, 42)
