41abce3dd518b62c9b27e979e9868d71b51c6c4e
Rachel Lewis <RLewis11769@gmail.com>
2021-12-13 19:49:32 +0000

    Add data for 5 - mock user login with login_as

---
+++ 0x01-i18n/5-app.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+""" Routes and config """
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """ App configuration """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ Locale selector """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+def get_user():
+    """ Get user from request """
+    id = request.args.get('login_as')
+    try:
+        return users.get(int(id))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ Before request to stash user in global """
+    g.user = get_user()
+
+
+@app.route("/", methods=['GET'])
+def index():
+    """ Index route """
+    return render_template("5-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
