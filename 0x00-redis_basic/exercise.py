ac5881c5131bf79144be3d5f1cb3303f638d59a9
Rachel Lewis <RLewis11769@gmail.com>
2021-12-14 21:43:37 +0000

    Define decorator method for counting - task 3

--- 0x00-redis_basic/exercise.py
+++ 0x00-redis_basic/exercise.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
 """ File to hold exercises to get more familiar with Redis """
+from functools import wraps
 from typing import Callable, Union
 from uuid import uuid4
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Counts the number of times a method is called
+    Returns method with count newly attached
+
+    Args:
+        method: method that is wrapped and counted
+    """
+
+    @wraps(method)
+    def wrapper(self, value):
+        """
+        Define wrapper to increment counter on each call
+
+        Args:
+            self: instance itself, so can access Redis instance
+            value: uh
+        """
+        # Qualname defines specific method
+        # Can differentiate between methods with same name based on location
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, value)
+    return wrapper
 
 
 class Cache():
@@ -13,6 +40,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in key:value pair in Redis database
@@ -28,13 +56,14 @@ class Cache():
     def get(self, key: str, fn: Callable = None) -> Union[str, int]:
         """
         Convert data to desired format and return it
-        Conserve original Redis.get behavior if key does not exist
+        Conserve original Redis.get behavior if function not passed
 
         Args:
             key: key to retrieve data from Redis database
             fn: optional function to call if data is not in Redis database
         """
         if fn:
+            # If string, essentially cast with str(key)
             return fn(self._redis.get(key))
         return self._redis.get(key)
 
@@ -43,7 +72,7 @@ class Cache():
         Return data in string format
 
         Args:
-            key: key to retrieve data from Redis database
+            key: key to cast as string
         """
         return self.get(key, str)
 
@@ -52,6 +81,6 @@ class Cache():
         Return data in int format
 
         Args:
-            key: key to retrieve data from Redis database
+            key: key to cast as int
         """
         return self.get(key, int)
