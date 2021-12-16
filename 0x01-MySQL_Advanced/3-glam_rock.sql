8665c13449990d915419a87b83bcd0e3fa570df1
Rachel Lewis <RLewis11769@gmail.com>
2021-12-16 15:21:38 +0000

    Add data for 3

---
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- List all bands with Glam Rock style by length of longevity
+-- Longevity calculated as split - formed (if null give default value of 2021)
+SELECT band_name, (IFNULL(split, 2021) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
