cbd0c40bb11cc9e5b703f83e6a280a9b4e366cd1
Rachel Lewis <RLewis11769@gmail.com>
2021-12-16 14:20:26 +0000

    Add data for task 0 - enum

---
+++ 0x01-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create users tables with id, email, name, country
+-- Doesn't fail if table exists, executed on any database
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+	PRIMARY KEY (id)
+);
