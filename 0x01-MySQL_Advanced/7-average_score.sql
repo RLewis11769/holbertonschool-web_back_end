6f3a22bb56773b581ad0e1ea37881a9be874fdf9
Rachel Lewis <RLewis11769@gmail.com>
2021-12-16 20:04:28 +0000

    Add data to 7-average_score and testing files

---
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,16 @@
+-- Create stored procedure that computes average score for all students for given student
+-- Takes user_id input parameter
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id int)
+BEGIN
+	SET @avg := (
+		SELECT AVG(score)
+		FROM corrections
+		WHERE user_id = user_id);
+	UPDATE users
+	SET average_score = @avg
+	WHERE id = user_id;
+END $$
+
+DELIMITER ;
