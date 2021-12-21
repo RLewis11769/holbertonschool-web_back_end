352a1740f4d653fe60e33192c598048b8cf2467e
Rachel Lewis <RLewis11769@gmail.com>
2021-12-21 01:42:20 +0000

    Add data to print for 12 - I hate pycodestyle for making me do it like this

---
+++ 0x02-NoSQL/12-log_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Provide stats about Nginx log files stored in MongoDB """
+from pymongo import MongoClient
+
+
+def stats():
+    """
+    Count documents in collection with:
+    - Database: logs
+    - Collection: nginx
+
+    Print:
+    - Total number of documents in given collection
+    - Number of documents with method ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    - Number of documents with method=GET and path=/status
+    """
+    # Connect to MongoDB
+    client = MongoClient()
+    # Get logs database
+    db = client.logs
+    # Get Nginx collection from logs database
+    collection = db.nginx
+
+    # Print number of total documents in the collection
+    print(f"{collection.count_documents({})} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    # For each method, print number of documents with that method
+    for method in methods:
+        print(f"\tmethod {method}: " +
+              f"{collection.count_documents({'method': method})}")
+
+    # Print number of documents with method=GET and path=/status
+    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})} \
+status check")
+
+
+if __name__ == "__main__":
+    stats()
