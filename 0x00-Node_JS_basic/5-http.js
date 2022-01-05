8abbfd0ac29179c6cab8768e1b9bd3b60368261c
Rachel Lewis <RLewis11769@gmail.com>
2022-01-05 05:14:11 -0600

    Account for bad db name - use then/catch for promise

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -9,27 +9,32 @@ const app = http
     if (req.url === '/') {
       res.end('Hello Holberton School!');
     } else if (req.url === '/students') {
-      const path = process.argv[2];
-      // Count students is returning a dictionary
-      const dict = await countStudents(path);
-      res.write('This is the list of our students\n');
-      // Key of each dict entry is the field to search
-      const fields = Object.keys(dict);
-      // Count all students in all fields
-      const total = fields.reduce((acc, curr) => acc + dict[curr].numStudents, 0);
-      res.write(`Number of students: ${total}\n`);
-      // Loop through each field
-      for (let x = 0; x < fields.length; x += 1) {
-        // Each dict field key has value of dict with numStudents and names as keys
-        res.write(`Number of students in ${fields[x]}: ${dict[fields[x]].numStudents}. `);
-        res.write(`List: ${dict[fields[x]].names.join(', ')}`);
-        // If not printing data for last field, add newline
-        if (x < fields.length - 1) {
-          res.write('\n');
-        }
-      }
-      // Need to end response manually bc dict
-      res.end();
+      // Using argv[2] as path to database file
+      await countStudents(process.argv[2])
+        .then((dict) => {
+          res.write('This is the list of our students\n');
+          // Key of each dict entry is the field to search
+          const fields = Object.keys(dict);
+          // Count all students in all fields
+          const total = fields.reduce((acc, curr) => acc + dict[curr].numStudents, 0);
+          res.write(`Number of students: ${total}\n`);
+          // Loop through each field
+          for (let x = 0; x < fields.length; x += 1) {
+            // Each dict field key has value of dict with numStudents and names as keys
+            res.write(`Number of students in ${fields[x]}: ${dict[fields[x]].numStudents}. `);
+            res.write(`List: ${dict[fields[x]].names.join(', ')}`);
+            // If not printing data for last field, add newline
+            if (x < fields.length - 1) {
+              res.write('\n');
+            }
+          }
+          // Need to end response manually bc dict
+          res.end();
+        })
+        .catch((error) => {
+          res.write(error.message);
+          res.end();
+        });
     }
   })
   .listen(1245);
