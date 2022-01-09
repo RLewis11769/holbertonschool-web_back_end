0aab52f3b2d539246e94cb37d75e8ed3ad391735
Rachel Lewis <RLewis11769@gmail.com>
2022-01-09 11:22:13 -0600

    Add data to 7 based on 5

--- 0x00-Node_JS_basic/7-http_express.js
+++ 0x00-Node_JS_basic/7-http_express.js
@@ -2,11 +2,36 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-module.exports = app = express();
+const app = express();
 app
   .get('/', (req, res) => res.send('Hello Holberton School!'))
   .get('/students', async (req, res) => {
-    const students = await countStudents(process.argv[2]);
-    res.send('This is the list of students: ' + students);
-    })
+    res.write('This is the list of our students\n');
+    await countStudents(process.argv[2])
+      .then((dict) => {
+        // Key of each dict entry is the field to search
+        const fields = Object.keys(dict);
+        // Count all students in all fields
+        const total = fields.reduce((acc, curr) => acc + dict[curr].numStudents, 0);
+        res.write(`Number of students: ${total}\n`);
+        // Loop through each field
+        for (let x = 0; x < fields.length; x += 1) {
+          // Each dict field key has value of dict with numStudents and names as keys
+          res.write(`Number of students in ${fields[x]}: ${dict[fields[x]].numStudents}. `);
+          res.write(`List: ${dict[fields[x]].names.join(', ')}`);
+          // If not printing data for last field, add newline
+          if (x < fields.length - 1) {
+            res.write('\n');
+          }
+        }
+      })
+      .catch((err) => {
+        res.write(err.message);
+      })
+      .finally(() => {
+        res.end();
+      });
+  })
   .listen(1245);
+
+module.exports = app;
