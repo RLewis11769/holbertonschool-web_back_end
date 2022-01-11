f0efaf91bf8b1549ff8287c2bc2ee5658d3a31e1
Rachel Lewis <RLewis11769@gmail.com>
2022-01-11 15:02:14 -0600

    Specify get method

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 describe ('Test suite', () => {
   // Should stub request in actual test
-  it('Test that app returns correct status code and result', (done) => {
-    request('http://localhost:7865/', (error, response, body) => {
+  it('Test that GET returns correct status code and result', (done) => {
+    request.get('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
       done();
