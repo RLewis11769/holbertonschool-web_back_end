5307c47918ec645eef5ed72b7f5228af0c52f3d9
Rachel Lewis <RLewis11769@gmail.com>
2022-01-11 00:13:44 -0600

    Add data for task 0

---
+++ 0x01-unittests_in_js/0-calcul.js
@@ -0,0 +1,8 @@
+// Function to be tested by 0-calcul.test.js
+
+const calculateNumber = (a, b) => {
+  // Round a and b and return sum
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
