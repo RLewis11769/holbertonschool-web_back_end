d9b75754b6eb90ef70ba0ad3e7e2a2ccfa0f8398
Rachel Lewis <RLewis11769@gmail.com>
2022-01-11 15:08:54 -0600

    Check request method is GET

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,14 @@
-const api = require('./api');
+// const api = require('./api');
 const { expect } = require('chai');
 const request = require('request');
 
 describe ('Test suite', () => {
   // Should stub request in actual test
   it('Test that GET returns correct status code and result', (done) => {
-    request.get('http://localhost:7865/', (error, response, body) => {
+    request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
+      expect(response.request.method).to.be.equal('GET');
       done();
     })
   });
