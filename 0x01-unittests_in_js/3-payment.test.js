f0c622e24bbea5355a5fe96159fc72aa26455791
Rachel Lewis <RLewis11769@gmail.com>
2022-01-11 13:49:58 -0600

    Add chai expect to track spy

--- 0x01-unittests_in_js/3-payment.test.js
+++ 0x01-unittests_in_js/3-payment.test.js
@@ -1,13 +1,14 @@
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const sinon = require('sinon');
+const { expect } = require('chai');
 
 describe ('Test suite', () => {
   it('Test that sendPaymentRequestToApi is same as Utils.calculateNumber SUM', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    spy.calledWith('SUM', 100, 20);
-    spy.calledOnce;
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     spy.restore();
   });
 })
