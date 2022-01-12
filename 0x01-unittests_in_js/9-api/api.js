e22f69c8001c039fc223bd58997db0e03a58a96a
Rachel Lewis <RLewis11769@gmail.com>
2022-01-12 17:59:12 -0600

    Change package.json test script

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -7,6 +7,9 @@ app.listen(7865, console.log(`API available on localhost port 7865`))
 
 app.get('/', (req, res) => res.end('Welcome to the payment system'))
 // Regex verification that id is a number of indeterminate length
-app.get('/cart/:id([0-9]*)', (req, res) => res.end(`Payment methods for cart ${req.params.id}`))
+app.get('/cart/:id([0-9]*)', (req, res) => {
+  const id = req.params.id;
+  res.end(`Payment methods for cart ${id}`)
+})
 
 module.exports = app;
