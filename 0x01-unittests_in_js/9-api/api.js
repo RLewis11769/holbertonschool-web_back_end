19fa7634a197298bdfdfae84015f543b49a683eb
Rachel Lewis <RLewis11769@gmail.com>
2022-01-12 17:47:25 -0600

    Rearrange api.js

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -3,16 +3,16 @@ const express = require('express');
 
 const app = express();
 
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
 app
   .get('/', (req, res) => {
     res.send('Welcome to the payment system');
   })
   // Regex verification that id is a number of indeterminate length
   .get('/cart/:id([0-9]*)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}`);
-  })
-  .listen(7865, () => {
-    console.log('API available on localhost port 7865');
+    res.send(`Payment methods for cart ${req.params.id}\n`);
   });
 
 module.exports = app;
