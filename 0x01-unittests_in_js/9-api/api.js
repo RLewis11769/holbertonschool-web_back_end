a342b0bf4e975fb3abbd1590b534ebe827aaf28e
Rachel Lewis <RLewis11769@gmail.com>
2022-01-12 17:51:02 -0600

    Rewrite api.js and update name on 9

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -3,16 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
-});
-app
-  .get('/', (req, res) => {
-    res.send('Welcome to the payment system');
-  })
-  // Regex verification that id is a number of indeterminate length
-  .get('/cart/:id([0-9]*)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params.id}\n`);
-  });
+app.listen(7865, console.log(`API available on localhost port 7865`))
+
+app.get('/', (req, res) => res.end('Welcome to the payment system'))
+// Regex verification that id is a number of indeterminate length
+app.get('/cart/:id([0-9]*)', (req, res) => res.end(`Payment methods for cart ${req.params.id}`))
 
 module.exports = app;
