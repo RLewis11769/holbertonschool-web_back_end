c559a1d42451b18f2c764fc8aecc32432a583e98
Rachel Lewis <RLewis11769@gmail.com>
2022-01-13 20:08:14 -0600

    Revert to orig except for bad import

--- 0x01-unittests_in_js/9-api/api.js
+++ 0x01-unittests_in_js/9-api/api.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 const app = express();
 
-app.listen(7865, console.log(`API available on localhost port 7865`))
-
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-// Regex verification that id is a number of indeterminate length
-app.get('/cart/:id([0-9]*)', (req, res) => {
-  const id = req.params.id;
-  res.end(`Payment methods for cart ${id}`)
-})
+app
+  .get('/', (req, res) => {
+    res.send('Welcome to the payment system');
+  })
+  .get('/cart/:id([0-9]*)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+  })
+  .listen(7865, () => console.log('API available on localhost port 7865'));
 
 module.exports = app;
--- 0x01-unittests_in_js/9-api/api.test.js
+++ 0x01-unittests_in_js/9-api/api.test.js
@@ -1,34 +1,31 @@
-const { expect } = require("chai");
-const request = require("request");
+const { expect } = require('chai');
+const request = require('request');
 
-describe("Test suite for /", () => {
+describe ('Test suite', () => {
   // Should stub request in actual test
-  it("Test that GET returns correct status code and result", (done) => {
-    request("http://localhost:7865/", (error, response, body) => {
+  it('Test that GET returns correct status code and result', (done) => {
+    request('http://localhost:7865/', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Welcome to the payment system");
-      expect(response.request.method).to.be.equal("GET");
+      expect(body).to.equal('Welcome to the payment system');
+      expect(response.request.method).to.be.equal('GET');
       done();
-    });
+    })
   });
-});
 
-describe("Test suite for /cart", () => {
-  it("Test that /cart/:id works with number", (done) => {
-    request("http://localhost:7865/cart/7", (error, response, body) => {
+  it('Test that /cart/:id works with number', (done) => {
+    request('http://localhost:7865/cart/7', (error, response, body) => {
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal("Payment methods for cart 7");
-      expect(response.request.method).to.be.equal("GET");
+      expect(body).to.equal('Payment methods for cart 7');
+      expect(response.request.method).to.be.equal('GET');
       done();
-    });
+    })
   });
-});
 
-describe("Test suite for /cart", () => {
-  it("Test that /cart/:id fails with non-number", (done) => {
-    request("http://localhost:7865/cart/fail", (error, response, body) => {
+  it('Test that /cart/:id fails with non-number', (done) => {
+    request('http://localhost:7865/cart/fail', (error, response, body) => {
       expect(response.statusCode).to.equal(404);
+      expect(response.request.method).to.be.equal('GET');
       done();
-    });
+    })
   });
-});
+})
