15889c96a70b86880b596fd0fc1c32f042759a71
Rachel Lewis <RLewis11769@gmail.com>
2022-01-20 19:09:06 -0600

    Add unittest for 8-job and add try/catch to 8 based on results

--- 0x02-queuing_system_in_js/8-job.js
+++ 0x02-queuing_system_in_js/8-job.js
@@ -3,20 +3,23 @@ const createPushNotificationsJobs = (jobs, queue) => {
   if (!Array.isArray(jobs)) throw Error('Jobs is not an array');
   for (const job of jobs) {
     const newJob = queue.create('push_notification_code_3', job).save();
-    newJob
-      .on('enqueue', () => {
-        console.log(`Notification job created: ${newJob.id}`);
-      })
-      .on('complete', () => {
-        console.log(`Notification job ${newJob.id} completed`);
-      })
-      .on('failed', (err) => {
-        console.log(`Notification job ${newJob.id} failed: ${err}`);
-      })
-      .on('progress', (progress) => {
-        console.log(`Notification job ${newJob.id} ${progress}% complete`);
-      });
-  }
+    try {
+      newJob
+        .on('enqueue', () => {
+          console.log(`Notification job created: ${newJob.id}`);
+        })
+        .on('complete', () => {
+          console.log(`Notification job ${newJob.id} completed`);
+        })
+        .on('failed', (err) => {
+          console.log(`Notification job ${newJob.id} failed: ${err}`);
+        })
+        .on('progress', (progress) => {
+          console.log(`Notification job ${newJob.id} ${progress}% complete`);
+        });
+    // Add empty catch block to prevent error from breaking the loop
+    } catch (err) {}
+  };
 };
 
 module.exports = createPushNotificationsJobs;
